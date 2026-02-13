984f3224214e4921a445a42db228502f37ee01c8
emty <workforemty@gmail.com>
2026-02-14 01:05:00 +0530

    Update AxeMod.java

--- src/main/java/com/axex/axe/AxeMod.java
+++ src/main/java/com/axex/axe/AxeMod.java
@@ -3,6 +3,7 @@ package com.axex.axe;
 import com.axex.axe.network.ModNetwork;
 import com.axex.axe.registry.ModEntities;
 import com.axex.axe.registry.ModItems;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,15 +11,19 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(AxeMod.MODID)
 public class AxeMod {
 
-    public static final String MODID = "axe_or_ex"; // ← use your real mod id
+    public static final String MODID = "axe_or_ex";
 
     public AxeMod() {
 
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModItems.register(modBus);
-        ModEntities.register(modBus);
+        // REGISTER DEFERRED REGISTERS PROPERLY
+        ModItems.ITEMS.register(modBus);
+        ModEntities.ENTITIES.register(modBus);
 
+        // REGISTER NETWORK
         ModNetwork.register();
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 }
