b1a2f6b4917699bc73d9832be4a5192fb86d4417
emty <workforemty@gmail.com>
2026-02-14 14:27:40 +0530

    Update ModItems.java

--- src/main/java/com/axex/axe/registry/ModItems.java
+++ src/main/java/com/axex/axe/registry/ModItems.java
@@ -6,12 +6,12 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public final class ModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AxeMod.MODID);
+public class ModItems {
 
-    public static final RegistryObject<Item> THROWABLE_AXE = ITEMS.register("throwable_axe",
-            () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, AxeMod.MODID);
 
-    private ModItems() {
-    }
+    public static final RegistryObject<Item> THROWABLE_AXE =
+            ITEMS.register("throwable_axe",
+                    () -> new Item(new Item.Properties().stacksTo(1)));
 }
